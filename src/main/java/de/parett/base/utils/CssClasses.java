f912c05a65ffd81350b292431823f21bae63fc6d
Paul Baatz <paul.baatz@siemens.com>
2024-03-20 17:21:49 +0100

    css classes

---
+++ src/main/java/de/parett/base/utils/CssClasses.java
@@ -0,0 +1,25 @@
+package de.parett.base.utils;
+
+public class CssClasses {
+
+	public static final String TABLE_PURE_STRIPED = "pure-table pure-table-bordered pure-table-striped";
+	public static final String TABLE_PURE_STRIPED_SEPARATE = "pure-table pure-table-bordered pure-table-striped border-separate";
+	public static final String TABLE_STICKY_HEADER = "sticky-header";
+	public static final String TABLE_STICKY_COLUMN = "sticky-column";
+
+	public static final String BUTTON_PURE = "pure-button";
+	public static final String BUTTON_PURE_PRIMARY = BUTTON_PURE + " pure-button-primary";
+	public static final String BUTTON_DELETE = "delete-button";
+
+	public static final String FORM_PURE = "pure-form";
+	public static final String FORM_PURE_ALIGNED = FORM_PURE + " pure-form-aligned";
+	public static final String FORM_PURE_CONTORL_GROUP = "pure-control-group";
+	public static final String FORM_PURE_CONTORLS = "pure-controls";
+	public static final String ICON_TRASH_BIN = "fa-regular fa-trash-can";
+	public static final String ICON_COPY = "fa-regular fa-copy";
+	public static final String ICON_X_MARK = "fa-solid fa-xmark";
+	public static final String ICON_CLIPBOARD = "fa-solid fa-clipboard";
+	public static final String ICON_PEN = "fa-solid fa-pen";
+
+
+}
